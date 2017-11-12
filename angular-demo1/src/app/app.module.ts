924b80cf9910b721590985c9c764eac41ed8a864
Steven Li <sli737@uwo.ca>
2017-11-12 18:00:51 +0000

    Completed the auto grow text input field and added it to my app

--- angular-demo1/src/app/app.module.ts
+++ angular-demo1/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { AutoGrowDirective } from './auto-grow.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    AutoGrowDirective
   ],
   imports: [
     BrowserModule
